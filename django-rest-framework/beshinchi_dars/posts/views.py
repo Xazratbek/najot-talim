eab20200824ea714ac1d969e6bd2166102c5e83a
Khazratbek <xazratbek123@gmail.com>
2026-04-30 17:47:00 +0500

    drf beshinchi dars vazifasi qilindi

--- django-rest-framework/beshinchi_dars/posts/views.py
+++ django-rest-framework/beshinchi_dars/posts/views.py
@@ -3,10 +3,17 @@ from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView,
 from rest_framework import status
 from .models import Post
 from .serializers import PostSerializer
+from django.db.models import Q
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        q = self.request.GET.get('q')
+        if q:
+            queryset = queryset.filter(Q(title__icontains=q) | Q(desc__icontains=q) | Q(author__icontains=q))
+        return queryset
 
 class PostRetrieveAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
